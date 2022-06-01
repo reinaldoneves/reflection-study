d91b5095c08b19eb6e533f75908236e941e3c198
reinaldoneves <reinaldo_neves@hotmail.com>
2022-06-01 16:36:03 -0300

    Discovering packages of a class with reflection

---
+++ src/br/com/reflectionstudy/fields/DiscoveryPackage.java
@@ -0,0 +1,19 @@
+package br.com.reflectionstudy.fields;
+
+import br.com.reflectionstudy.model.Goat;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DiscoveryPackage {
+
+    @Test
+    public void givenClass_whenGetsPackageInfo_thenCorrect() {
+        Goat goat = new Goat("goat");
+        Class<?> goatClass = goat.getClass();
+        Package pkg = goatClass.getPackage();
+
+        assertEquals("br.com.reflectionstudy.model", pkg.getName());
+    }
+
+}
