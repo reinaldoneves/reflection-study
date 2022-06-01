e0733246fbb6c4fec8aea7a45c6c03cd2e103d04
reinaldoneves <reinaldo_neves@hotmail.com>
2022-06-01 16:37:18 -0300

    Discovering packages of a class with reflection

--- src/br/com/reflectionstudy/fields/DiscoveryPackage.java
+++ src/br/com/reflectionstudy/fields/DiscoveryPackage.java
@@ -2,7 +2,6 @@ package br.com.reflectionstudy.fields;
 
 import br.com.reflectionstudy.model.Goat;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DiscoveryPackage {
